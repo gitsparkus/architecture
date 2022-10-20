2394e4fc1704cb22adf5238fc1b8177bc19817fe
Alexandr <aleksandrov.a.a@gmail.com>
2022-10-20 12:33:15 +0200

    dev

--- hero.py
+++ hero.py
@@ -1,25 +1,51 @@
 from abc import ABC, abstractmethod
-from __future__ import annotations
+from armor_factory import *
+from weapon_factory import *
 
 
 class Hero(ABC):
 
-    def __int__(self, name: str, health: int):
+    def __init__(self, name: str):
+        self.armor = None
+        self.weapon = None
         self.name = name
-        self.health = health
+        self.health = 100
+        self.get_equipment()
 
     @abstractmethod
-    def attak(self, antagonist: Hero):
+    def get_equipment(self):
         pass
+
+    def __str__(self):
+        return f"Имя: {self.name}\nЗдоровье:{self.health}\nОружие:\n{self.weapon}\nБроня:\n{self.armor}"
 
 
 class Archer(Hero):
-    ...
+
+    def __init__(self, name: str):
+        super().__init__(name)
+
+    def get_equipment(self):
+        self.weapon = WeaponFactory.create_weapon(WeaponType.BOW)
+        self.armor = ArmorFactory.create_armor(ArmorType.LEATHER_ARMOR)
 
 
-class Khight(Hero):
-    ...
+class Knight(Hero):
+    def __init__(self, name: str):
+        super().__init__(name)
+
+    def get_equipment(self):
+        self.weapon = WeaponFactory.create_weapon(WeaponType.SWORD)
+        self.armor = ArmorFactory.create_armor(ArmorType.CHAIN_ARMOR)
 
 
+if __name__ == '__main__':
+    knight = Knight('Arthur')
+    archer = Archer('Серёжа')
 
+    print(knight, '\n\n')
+    print(archer)
 
+    print(knight, '\n\n')
+    WeaponFactory.upgrade_weapon(archer.weapon)
+    print(archer)
--- weapon_factory.py
+++ weapon_factory.py
@@ -1,40 +1,86 @@
 from __future__ import annotations
 from abc import ABC, abstractmethod
+from enum import Enum
+
+
+class WeaponType(Enum):
+    SWORD = 1
+    BOW = 2
+
+
+class WeaponClass(Enum):
+    STANDARD = 1
+    LEGENDARY = 2
 
 
 class Weapon(ABC):
 
+    def __init__(self, weapon_type: WeaponType, level: int = 0):
+        self.level = level
+        self.weapon_class = WeaponClass.STANDARD
+        self.weapon_type = weapon_type
+        self._upgrade_if_level()
+
+    def level_up(self):
+        self.level += 1
+        self._upgrade_if_level()
+
     @abstractmethod
-    def upgrade(self):
+    def _upgrade_if_level(self):
         pass
 
+    def _upgrade(self):
+        if self.weapon_class != WeaponClass.LEGENDARY:
+            self.weapon_class = WeaponClass.LEGENDARY
 
-class WeaponFactory(ABC):
-
-    @abstractmethod
-    def create_weapon(self):
-        pass
-
-    def upgrade_weapon(self, weapon: Weapon):
-        weapon.upgrade()
+    def __str__(self) -> str:
+        return f"Тип: {self.weapon_type.name}\nКласс: {self.weapon_class.name} \nУровернь: {self.level}"
 
 
 class Sword(Weapon):
-    def upgrade(self) -> str:
-        return "{Result of the ConcreteProduct1}"
+
+    def __init__(self, level: int = 0):
+        super().__init__(WeaponType.SWORD, level)
+
+    def _upgrade_if_level(self):
+        if self.level >= 10:
+            self._upgrade()
 
 
 class Bow(Weapon):
-    def upgrade(self) -> str:
-        return "{Result of the ConcreteProduct2}"
+
+    def __init__(self, level: int = 0):
+        super().__init__(WeaponType.BOW, level)
+
+    def _upgrade_if_level(self):
+        if self.level >= 5:
+            self._upgrade()
 
 
-class SwordFactory(WeaponFactory):
+class WeaponFactory:
 
-    def create_weapon(self) -> Weapon:
-        return Sword()
+    @staticmethod
+    def create_weapon(weapon_type: WeaponType, level: int = 0) -> Weapon:
+        if weapon_type == WeaponType.SWORD:
+            return Sword(level)
+        elif weapon_type == WeaponType.BOW:
+            return Bow(level)
+
+    @staticmethod
+    def upgrade_weapon(weapon: Weapon):
+        weapon._upgrade()
 
 
-class BowFactory(WeaponFactory):
-    def create_weapon(self) -> Weapon:
-        return Bow()
+if __name__ == '__main__':
+    bow = WeaponFactory.create_weapon(WeaponType.BOW)
+
+    print(bow)
+    bow.level_up()
+    print(bow)
+    WeaponFactory.upgrade_weapon(bow)
+    print(bow)
+
+    sword = WeaponFactory.create_weapon(WeaponType.SWORD,10)
+    print(sword)
+    sword.level_up()
+    print(sword)
