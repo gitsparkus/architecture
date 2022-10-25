f4d848f62aab7ec95d3c32763a397f6488fd6bc3
Alexandr <aleksandrov.a.a@gmail.com>
2022-10-25 17:58:33 +0200

    fix

--- lesson3.py
+++ lesson3.py
@@ -15,14 +15,16 @@
 """
 
 from abc import ABC, abstractmethod
-from math import pi
+from math import pi, tan, radians
 
 
 class ArgumentTypeError(ValueError):
     pass
 
+
 class ArgumentValueError(ValueError):
     pass
+
 
 class GeometryError(ValueError):
     pass
@@ -39,6 +41,60 @@ class Shape(ABC):
         pass
 
 
+class Polygon(Shape):
+
+    def __init__(self, *args: float):
+        self._sides = args
+
+    @abstractmethod
+    def get_area(self) -> float:
+        pass
+
+    def get_perimeter(self) -> float:
+        return sum(self._sides)
+
+
+class Triangle(Polygon):
+
+    def __init__(self, a: float, b: float, c: float):
+        if a + b <= c or a + c <= b or b + c <= a:
+            raise GeometryError('Из переданных отрезков невозможно собрать треугольник!')
+        super().__init__(a, b, c)
+
+    def get_area(self) -> float:
+        p = self.get_perimeter() / 2
+        return (p * (p - self._sides[0]) * (p - self._sides[1]) * (p - self._sides[2])) ** 0.5
+
+
+class Rectangle(Polygon):
+
+    def __init__(self, a: float, b: float):
+        super().__init__(a, b, a, b)
+
+    def get_area(self) -> float:
+        return self._sides[0] * self._sides[1]
+
+
+class Square(Rectangle):
+
+    def __init__(self, a: float):
+        super().__init__(a, a)
+
+
+class Circle(Shape):
+
+    def __init__(self, r: float):
+        if r <= 0:
+            raise ArgumentValueError('Радиус должен быть больше 0!')
+        self.radius = r
+
+    def get_area(self) -> float:
+        return pi * (self.radius ** 2)
+
+    def get_perimeter(self) -> float:
+        return 2 * pi * self.radius
+
+
 class ShapeList(list):
 
     def append(self, shape: Shape):
@@ -53,73 +109,20 @@ class ShapeList(list):
         return sum((shape.get_area() for shape in self))
 
 
-class Triangle(Shape):
-
-    def __init__(self, a: float, b: float, c: float):
-
-        if a <= 0 or b <= 0 or c <= 0:
-            raise ArgumentValueError('Все стороны фигуры должны быть больше 0!')
-        if a + b <= c or a + c <= b or b + c <= a:
-            raise GeometryError('Из переданных отрезков невозможно собрать треугольник!')
-        self.__a = a
-        self.__b = b
-        self.__c = c
-
-    def get_area(self) -> float:
-        p = self.get_perimeter() / 2
-        return (p * (p - self.__a) * (p - self.__b) * (p - self.__c)) ** 0.5
-
-    def get_perimeter(self) -> float:
-        return self.__a + self.__b + self.__c
-
-
-class Circle(Shape):
-
-    def __init__(self, r: float):
-        if r <= 0:
-            raise ArgumentValueError('Радиус должен быть больше 0!')
-        self.__r = r
-
-    def get_area(self) -> float:
-        return pi * (self.__r ** 2)
-
-    def get_perimeter(self) -> float:
-        return 2 * pi * self.__r
-
-
-class Rectangle(Shape):
-
-    def __init__(self, a: float, b: float):
-        if a <= 0 or b <= 0:
-            raise ArgumentValueError('Сторона фигуры не может быть меньше 0!')
-        self.__a = a
-        self.__b = b
-
-    def get_area(self) -> float:
-        return self.__a * self.__b
-
-    def get_perimeter(self) -> float:
-        return 2 * (self.__a + self.__b)
-
-
-class Square(Rectangle):
-
-    def __init__(self, a: float):
-        super().__init__(a, a)
-
-
 if __name__ == '__main__':
     shapes_list = ShapeList()
 
-    shapes_list.append(Triangle(1, 1, 1))
-    shapes_list.append(Square(12))
-    shapes_list.append(Rectangle(5, 6))
-    shapes_list.append(Circle(4))
+    triangle = Triangle(1, 1, 1)
+    square = Square(12)
+    rectangle = Rectangle(5, 6)
+    circle = Circle(4)
 
-    sum_perimeter = sum((shape.get_perimeter() for shape in shapes_list))
-    print(sum_perimeter)
-    print(shapes_list.get_sum_perimeter())
+    shapes_list.append(triangle)
+    shapes_list.append(square)
+    shapes_list.append(rectangle)
+    shapes_list.append(circle)
 
-    sum_area = sum((shape.get_area() for shape in shapes_list))
-    print(sum_area)
-    print(shapes_list.get_sum_area())
+    for item in shapes_list:
+        print(item.__class__)
+        print(item.get_area())
+
